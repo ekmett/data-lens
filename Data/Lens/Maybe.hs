ee2420d3d7c015ba166a12c47e4b8972688505f0
Tony Morris <tmorris@tmorris.net>
2012-01-13 16:59:57 +1000

    lenses that project onto Maybe values

---
+++ Data/Lens/Maybe.hs
@@ -0,0 +1,81 @@
+-- | Lenses that project on to a @Maybe@ value.
+module Data.Lens.Maybe
+  ( MLens
+  -- * Functional API
+  , mgetL
+  , mset
+  , unset
+  -- * Operator API
+  , (^|), (^|!)   -- getter
+  , (^|=), (^|!=) -- setter
+  ) where
+
+import Data.Lens.Common
+import Data.Maybe
+
+-- | 
+type MLens a b =
+  Lens a (Maybe b)
+
+-- | Gets the @Just@ value from a lens or a default if @Nothing@.
+mgetL ::
+  MLens a b
+  -> a
+  -> b -- ^ The default if @Nothing@.
+  -> b
+mgetL =
+  (^|)
+
+-- | Gets the @Just@ value from a lens or a default if @Nothing@.
+(^|) ::
+  MLens a b
+  -> a
+  -> b -- ^ The default if @Nothing@.
+  -> b
+l ^| a =
+  flip fromMaybe (l ^$ a)
+
+-- | Gets the @Just@ value from a lens or a default if @Nothing@.
+(^|!) ::
+  MLens a b
+  -> a
+  -> b -- ^ The default if @Nothing@.
+  -> b
+l ^|! a =
+  flip fromMaybe (l ^$! a)
+
+-- | Sets a @Just@ value on a lens.
+mset ::
+  MLens a b
+  -> b -- ^ The value to set.
+  -> a
+  -> a
+mset =
+  (^|=)
+
+-- | Sets a @Just@ value on a lens.
+(^|=) ::
+  MLens a b
+  -> b -- ^ The value to set.
+  -> a
+  -> a
+l ^|= b =
+  l ^= Just b
+
+-- | Sets a @Just@ value on a lens.
+(^|!=) ::
+  MLens a b
+  -> b -- ^ The value to set.
+  -> a
+  -> a
+l ^|!= b =
+  l ^!= Just b
+
+-- | Sets a @Nothing@ value on a lens.
+unset ::
+  MLens a b
+  -> a
+  -> a
+unset l =
+  l ^= Nothing
+
